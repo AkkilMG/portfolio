4c3c971a082ef0f396152a0a76e128bb442af73f
Akkil MG <akkilcharanmg@gmail.com>
2024-10-16 23:57:54 +0530

    Trying

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,57 @@
 import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import "./globals.css";
+import { siteConfig } from "@/lib/siteConfig";
 
 export const metadata: Metadata = {
-  title: "Akkil M G | Portfolio",
-  description: "This is a portfolio website of Akkil M G"
+  title: siteConfig.name,
+  description: siteConfig.description,
+  keywords: [
+    "Akkil M G",
+    "Akkil MG",
+    "Frontend Developer",
+    "Backend Developer",
+    "API Developer",
+    "FARM Stack Developer",
+    "MERN Stack Developer",
+    "Automation Developer",
+    "Akkil",
+  ],
+  creator: "AkkilMG",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@AkkilMG",
+  },
+  authors: [
+    {
+      name: "Akkil M G",
+      url: siteConfig.url,
+    },
+  ],
+  icons: {
+    icon: "/site/favicon.ico",
+    shortcut: "/site/favicon-16x16.png",
+    apple: "/site/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
---
+++ src/lib/siteConfig.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+    name: "Akkil M G | Portfolio",
+    description:
+      "Portfolio Site of Deveesh Shetty, a Frontend Developer showcasing his projects and experiences",
+    url: "https://akkilmg.vercel.app",
+    ogImage: "https://deveesh.vercel.app/site/og.png",
+    socials: {
+      twitter: "https://twitter.com/AkkilMG",
+      github: "https://github.com/AkkilMG",
+      linkedin: "https://www.linkedin.com/in/AkkilMG/"
+    },
+  }
